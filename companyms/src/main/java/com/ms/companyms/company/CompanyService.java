b60f9867b9f01607f5f6618ce6d736728ecbfe16
alexandrucristian3012 <alexandru.cristian.30.12@gmail.com>
2025-09-24 14:56:04 +0300

    - added/extracted company package as a microservice - replaced h2 database with postgres for microservices

---
+++ companyms/src/main/java/com/ms/companyms/company/CompanyService.java
@@ -0,0 +1,12 @@
+package com.ms.companyms.company;
+
+import java.util.List;
+
+
+public interface CompanyService {
+	public List<Company> getAllCompanies();
+	public void createCompany( Company company);
+	public Company getCompanyById(Long id);
+	public boolean deleteCompanyById(Long id);
+	public boolean updateCompany(Long id, Company job);
+}
